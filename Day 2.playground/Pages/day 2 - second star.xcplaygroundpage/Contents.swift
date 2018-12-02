0d0e0737adcf63ee82ac92f64d7a6ac14031f9b5
Jon Friskics <jonfriskics@gmail.com>
2018-12-02 01:25:34 -0500

    day 2 - second star solution

--- Day 2.playground/Pages/day 2 - second star.xcplaygroundpage/Contents.swift
+++ Day 2.playground/Pages/day 2 - second star.xcplaygroundpage/Contents.swift
@@ -1,9 +1,22 @@
 import Foundation
 import PlaygroundSupport
 
-// Day 2 - first star
+// Day 2 - second star
 
 guard let fileUrl = Bundle.main.url(forResource: "day2_input", withExtension: "txt") else { fatalError("Can't find file") }
 let text = try String(contentsOf: fileUrl, encoding: String.Encoding.utf8)
-let array = text.components(separatedBy: "\n").map { Int($0)! }
+let array = text.components(separatedBy: "\n")
 
+var candidates: [String] = []
+
+for i in 0...array.count-1 {
+  for j in 0...array.count-1 {
+    let hamming = zip(array[j],array[i]).filter{ $0 != $1 }.count
+    if(hamming == 1) {
+      candidates.append(array[i])
+    }
+  }
+}
+
+let same = zip(candidates[0],candidates[1]).filter{ $0 == $1 }
+print(same.compactMap({ String($0.0) }).joined(separator: ""))
