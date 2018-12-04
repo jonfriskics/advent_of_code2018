221220bf452a32902486d167e8d39945a2966ad1
Jon Friskics <jonfriskics@gmail.com>
2018-12-03 23:06:33 -0500

    prep day 4

---
+++ Day4.playground/Pages/day4 - first star.xcplaygroundpage/Contents.swift
@@ -0,0 +1,8 @@
+import Foundation
+import PlaygroundSupport
+
+// Day 4 - first star
+
+guard let fileUrl = Bundle.main.url(forResource: "day4_input", withExtension: "txt") else { fatalError("Can't find file") }
+let text = try String(contentsOf: fileUrl, encoding: String.Encoding.utf8)
+let array = text.components(separatedBy: "\n")
