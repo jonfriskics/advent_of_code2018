76434d7a348d14093f332cbed85d05bca8b22b14
Jon Friskics <jonfriskics@gmail.com>
2018-12-09 23:12:09 -0500

    add day9 placeholder

---
+++ Day 9.playground/Pages/day 9 - second star.xcplaygroundpage/Contents.swift
@@ -0,0 +1,9 @@
+import Foundation
+import PlaygroundSupport
+
+// Day 9 - second star
+
+guard let fileUrl = Bundle.main.url(forResource: "day9_input", withExtension: "txt") else { fatalError("Can't find file") }
+let text = try String(contentsOf: fileUrl, encoding: String.Encoding.utf8)
+var characters = Array(text)
+characters.removeLast()
