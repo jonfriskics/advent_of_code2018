48e0a0575d179f2f259241c97bb8b366eb5bf272
Jon Friskics <jonfriskics@gmail.com>
2018-12-11 20:09:11 -0500

    more hacking on day 5

--- Day 5.playground/Pages/day5 - second star.xcplaygroundpage/Contents.swift
+++ Day 5.playground/Pages/day5 - second star.xcplaygroundpage/Contents.swift
@@ -4,6 +4,38 @@ import PlaygroundSupport
 // Day 5 - second star
 
 guard let fileUrl = Bundle.main.url(forResource: "day5_input", withExtension: "txt") else { fatalError("Can't find file") }
-let text = try String(contentsOf: fileUrl, encoding: String.Encoding.utf8)
+var text = try String(contentsOf: fileUrl, encoding: String.Encoding.utf8)
+
+let letters: Set<Character> = ["A", "a"]
+text.removeAll(where: { letters.contains($0) })
+
 var characters = Array(text)
+characters.count
+characters.last
 characters.removeLast()
+
+func matchingLettersPair(char1: Character, char2: Character) -> Bool {
+  let string1 = String(char1)
+  let string2 = String(char2)
+  if(string1 == string2.uppercased() || string1.uppercased() == string2) {
+    return true
+  }
+  return false
+}
+
+func iterateArray(chars: [Character]) {
+  var cs = chars
+  loop: for i in 0..<chars.count {
+    if(i == cs.count - 1) {
+      print("in conditional cs: \(cs), i: \(i), cs_count: \(cs.count), cs_count_minus_one: \(cs.count-1)")
+      fatalError()
+    }
+    if(matchingLettersPair(char1: cs[i], char2: cs[i+1])) {
+      cs.removeSubrange(i...i+1)
+      iterateArray(chars: cs)
+      break loop
+    }
+  }
+}
+
+iterateArray(chars: characters)
